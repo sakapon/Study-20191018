a835c27327f22d97b7c20210cfe262350394ba2b
Keiho Sakapon <sakapon@users.noreply.github.com>
2019-10-18 17:47:49 +0900

    Update Program.cs

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,7 +6,9 @@ namespace ConsoleApp1
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			Console.WriteLine("Hello GitHub!");
+			Console.WriteLine("Hello GitHub!");
+			Console.WriteLine("Hello GitHub!");
 		}
 	}
 }
